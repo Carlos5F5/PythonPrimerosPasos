589b07bda48d6ba8cd040ef13e71630c491b9d42
Carlos5f5 <carlossebas.873@gmail.com>
2024-12-09 19:37:23 -0500

    Bucles

---
+++ matchSwitch.py
@@ -0,0 +1,19 @@
+mes = 5
+
+match mes:
+    case "Enero":
+        print("Enero es el primer mes")
+    case "Febrero":
+        print("Febrero es el segundo mes")
+    case "Marzo":
+        print("Marzo es el tercer mes")
+    case "Abril":
+        print("Abril es el cuarto mes")
+    case 5:
+        print("Mayo es el quinto mes")
+    case "Junio":
+        print("Junio es el sexto mes")
+    case "Julio":
+        print("Julio es el septimo mes")
+    case "Agosto":
+        print("Agosto es el octavo mes")
